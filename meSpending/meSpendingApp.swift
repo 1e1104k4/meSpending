fe6f32d70cca647efa362617beb6db3371f79e51
1e1104k4 <1e1104k4@gmail.com>
2023-12-16 09:54:46 -0600

    Initial Commit

---
+++ meSpending/meSpendingApp.swift
@@ -0,0 +1,17 @@
+//
+//  meSpendingApp.swift
+//  meSpending
+//
+//  Created by Leila on 12/16/23.
+//
+
+import SwiftUI
+
+@main
+struct meSpendingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
