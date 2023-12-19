2a7d22c60559b02de50b61ce7e2be0a752a909f8
1e1104k4 <1e1104k4@gmail.com>
2023-12-19 12:35:57 -0600

    feat: extension on View with expenseStyle

--- View-ExpenseStyle.swift
+++ View-ExpenseStyle.swift
@@ -5,4 +5,16 @@
 //  Created by Leila on 12/19/23.
 //
 
-import Foundation
+import SwiftUI
+
+extension View {
+    func expenseStyle(for item: ExpenseItem) -> some View {
+        if item.amount < 10 {
+            return self.font(.body)
+        } else if item.amount < 100 {
+            return self.font(.title3)
+        } else {
+            return self.font(.title2)
+        }
+    }
+}
