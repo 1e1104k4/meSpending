682960765967521b70c755df66735f73ccc9a2f9
leila <143362178+1e1104k4@users.noreply.github.com>
2023-12-20 14:08:57 -0600

    new view for expense section

---
+++ meSpending/ExpenseSection.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseSection.swift
+//  meSpending
+//
+//  Created by Leila on 12/20/23.
+//
+
+import SwiftUI
+
+struct ExpenseSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseSection()
+}
