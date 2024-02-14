5fefb9b78ba9d80a270090d1957cc4fcb56a8970
leila <143362178+1e1104k4@users.noreply.github.com>
2024-02-14 12:42:33 -0600

    delete Expenses class

--- meSpending/ContentView.swift
+++ meSpending/ContentView.swift
@@ -12,25 +12,6 @@ struct ExpenseItem: Identifiable, Codable, Equatable {
     let name: String
     let type: String
     let amount: Double
-}
-
-@Observable
-class Expenses {
-    var items = [ExpenseItem]() {
-        didSet {
-            if let encoded = try? JSONEncoder().encode(items) {
-                UserDefaults.standard.set(encoded, forKey: "Items")
-            }
-        }
-    }
-    
-    var personalItems: [ExpenseItem] {
-        items.filter { $0.type == "Personal" }
-    }
-    
-    var businessItems: [ExpenseItem] {
-        items.filter { $0.type == "Business" }
-    }
     
     init() {
         if let savedItems = UserDefaults.standard.data(forKey: "Items") {
